371c7394095a2d265fcbdd13ed762658f44cc258
Hayk Avetisyan <haykave@yahoo.com>
2026-04-02 14:20:55 +0400

    Configure Docker with Nginx, Postgres, and Media/Static volumes02.04.26

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -32,8 +32,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "haevolutionha@gmail.com"
-EMAIL_SUPPORT_USER = "frunzik.aristakesyan44@gmail.com"
+EMAIL_HOST_USER = "avehaykave@gmail.com"
+EMAIL_SUPPORT_USER = "avehaykave@gmail.com"
 EMAIL_HOST_PASSWORD = 'pmhxtlyaghgdpfze'
 
 
